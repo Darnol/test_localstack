6dcd777d28debfae133be7a15441900054f5422a
Darnol <32634736+Darnol@users.noreply.github.com>
2024-03-12 18:00:17 +0100

    WIP : Explore api gateway deploying resources with multiple methods

--- minimal-backend-service/tmp_lambda.py
+++ minimal-backend-service/tmp_lambda.py
@@ -1,7 +1,14 @@
 def handler(event, context):
+    print("THIS IS THE EVENT\n")
     print(event)
+    print("-------------------")
+
+    print("NOW WE PROCESS event\n")
+    body = event['body']
+    url_params = event['queryStringParameters']
+
     return({
         "statusCode":200,
         "headers":{},
-        "body":"THISISTHEBODY"
+        "body":{"key1":"value1"}
     })
--- minimal-backend-service/tmp_test_boto.py
+++ minimal-backend-service/tmp_test_boto.py
@@ -35,19 +35,42 @@ def deploy_lambda(fct_name: str):
         Code = {'ZipFile': open(ZIP_NAME, 'rb').read()}
         )
     
-deploy_lambda(fct_name="lambda_test2")
- 
 def create_api(api_name: str):
     """
     Wrapper to create API Gateway
     """
     # Create the REST API
     apig_rest_api = apig_client.create_rest_api(name = api_name)
-create_api("api_test2")
+    return apig_rest_api
 
-def add_lambda_integration(api_name: str, lambda_name: str, resource_path: str):
+def create_resource(api_name: str, resource_pathPart: str, resource_path: str):
+    "Wrapper for creating a resource"
+
+    # Get API ID
+    try:
+        api_id = [x['id'] for x in apig_client.get_rest_apis()['items'] if x['name'] == api_name][0]
+    except IndexError:
+        raise ValueError(f"api {api_name} not found")
+    
+    # Check if a resource with the same path already exists
+    TODO : Do not implement a resource with the exact same path twice!
+    
+    # Fetch all resources
+    apig_resources = apig_client.get_resources(restApiId = api_id)
+    pp.pprint(apig_resources)
+    
+    # Add resource
+    apig_new_resource = apig_client.create_resource(
+        restApiId = api_id,
+        parentId = apig_resources['items'][0]['id'],
+        pathPart = "debugcall"
+    )
+
+    return apig_new_resource
+
+def add_lambda_method_and_integration_to_resource(api_name: str, resource_path: str, lambda_name: str, method: str):
     """
-    Wrapper to create an integration to a lambda function for a given api gateway
+    Wrapper to add a method to an existing resource
     """
 
     # Get API ID
@@ -55,25 +78,25 @@ def add_lambda_integration(api_name: str, lambda_name: str, resource_path: str):
         api_id = [x['id'] for x in apig_client.get_rest_apis()['items'] if x['name'] == api_name][0]
     except IndexError:
         raise ValueError(f"api {api_name} not found")
+    
+    # Get Resource ID, error if not found
+    res_found = False
+    for res in [x for x in apig_client.get_resources(restApiId = api_id)['items']]:
+        if res.get("pathPart","") == resource_path:
+            res_found = True
+            res_id = res['id']
+            break
+    if not res_found:
+        raise ValueError(f"Did not find resource with path {resource_path}")
 
     # Get the ARN of the desired lambda function to integrate
     lambda_arn = [function_def['FunctionArn'] for function_def in lambda_client.list_functions()['Functions'] if function_def['FunctionName']==lambda_name][0]
 
-    # Fetch all resources
-    apig_resources = apig_client.get_resources(restApiId = api_id)
-    
-    # Add resource
-    apig_new_resource = apig_client.create_resource(
-        restApiId = api_id,
-        parentId = apig_resources['items'][0]['id'],
-        pathPart = resource_path
-    )
-
     # Put a HTTP method to a resource
     apig_new_method = apig_client.put_method(
         restApiId = api_id,
-        resourceId = apig_new_resource['id'],
-        httpMethod = "GET", # The request to call a lambda function is always a GET?
+        resourceId = res_id,
+        httpMethod = method,
         authorizationType = "NONE",
         apiKeyRequired = False
     )
@@ -81,16 +104,14 @@ def add_lambda_integration(api_name: str, lambda_name: str, resource_path: str):
     # Put an integration
     apig_new_integration = apig_client.put_integration(
         restApiId = api_id,
-        resourceId = apig_new_resource['id'],
-        httpMethod = "GET",
+        resourceId = res_id,
+        httpMethod = method,
         type = "AWS_PROXY", # I think thats given if we use lambda
-        integrationHttpMethod = "GET", # Must match the lambda function?
+        integrationHttpMethod = "POST", # I think this is always POST, we forward the request to the lambda function
         uri = f"arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/{lambda_arn}/invocations"
     )
-    
-    # TODO: Integration response aka what field should be returned from the lambda handler??
 
-add_lambda_integration(api_name = "api_test2", lambda_name="lambda_test2", resource_path="testcall")
+    return apig_new_method, apig_new_integration
 
 def deploy_api(api_name: str):
     """
@@ -110,16 +131,7 @@ def deploy_api(api_name: str):
         stageName = api_name
     )
 
-deploy_api(api_name = "api_test2")
-
-# Ressrouce path id
-api_id = [x['id'] for x in apig_client.get_rest_apis()['items'] if x['name'] == "api_test2"][0]
-resource_path_id = [x['id'] for x in apig_client.get_resources(restApiId = api_id)['items'] if x.get('pathPart','') == "testcall"]
-
-# Stage
-api_deployment_ids = [x['id'] for x in apig_client.get_deployments(restApiId=api_id)['items']]
-for deployment_id in api_deployment_ids:
-    pp.pprint(apig_client.get_stages(restApiId = api_id, deploymentId=deployment_id))
+    return apig_deployment
 
 def get_resource_path(api_name: str, resource_path: str) -> str:
     """
@@ -158,5 +170,39 @@ def get_resource_path(api_name: str, resource_path: str) -> str:
 
     return url
 
-get_resource_path(api_name = "api_test2", resource_path="testcall")
-copy(get_resource_path(api_name = "api_test2", resource_path="testcall"))
+
+
+
+###
+# Deploy
+
+tag_to_use = "1"
+tag_resource_pathPart = "testcall" ; tag_resource_path = "/testcall"
+tag_resource_parametrized_pathPart = "anothertestcall" ; tag_resource_parametrized_path = "/anothertestcall/{test_id}"
+
+deploy_lambda(fct_name=f"lambda_test_{tag_to_use}")
+
+response_create_api = create_api(f"api_test_{tag_to_use}")
+pp.pprint(response_create_api)
+
+# Create two resources, one normal and one parametrized
+# api_name = f"api_test_{tag_to_use}" ; resource_pathPart = tag_resource_pathPart ; resource_path = tag_resource_path
+response_create_resrouce = create_resource(f"api_test_{tag_to_use}", tag_resource_pathPart, tag_resource_path)
+pp.pprint(response_create_resrouce)
+
+# response_create_resrouce_parametrized = create_resource(f"api_test_{tag_to_use}", tag_resource_parametrized)
+# pp.pprint(response_create_resrouce_parametrized)
+
+# lambda_name = f"lambda_test_{tag_to_use}"
+response_create_method, response_create_integration = add_lambda_method_and_integration_to_resource(
+    api_name = f"api_test_{tag_to_use}",
+    lambda_name=f"lambda_test_{tag_to_use}",
+    resource_path=tag_resource,
+    method="GET"
+    )
+pp.pprint(response_create_method)
+pp.pprint(response_create_integration)
+
+response_deploy_api = deploy_api(api_name = f"api_test_{tag_to_use}")
+get_resource_path(api_name = f"api_test_{tag_to_use}", resource_path=tag_resource)
+copy(get_resource_path(api_name = f"api_test_{tag_to_use}", resource_path=tag_resource))
